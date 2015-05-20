2b7d35330612c6994ed438c6cfa3880e339d7c74
user_39 <vad0zaa@mail.ru>
2015-05-20 18:50:00 +0300

    first commit , working code

--- app/src/main/java/ee/sinchukov/ecbrates/CurrenciesListActivity.java
+++ app/src/main/java/ee/sinchukov/ecbrates/CurrenciesListActivity.java
@@ -1,39 +1,28 @@
 package ee.sinchukov.ecbrates;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import ee.sinchukov.ecbrates.HandleXML;
 
 
-public class CurrenciesListActivity extends ActionBarActivity {
+public class CurrenciesListActivity extends ListActivity {
+
+    ArrayList<String> currenciesList = new ArrayList<String>();
+    HandleXML handleXML;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currencies_list);
+        handleXML=new HandleXML("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
+        handleXML.fetchXML();
+        while(handleXML.parsingComplete);
+        currenciesList=handleXML.getRatesList();
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, currenciesList));
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_currencies_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
